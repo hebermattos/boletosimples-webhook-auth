aef6f937ebd5af0ec259440a4e2ad7b35a3d8916
Heber Mattos <hebermattos@outlook.com>
2018-07-19 17:40:00 -0300

    error handling

--- index.php
+++ index.php
@@ -8,6 +8,14 @@ $app->post('/webhook-auth', function () use ($app) {
 
     $authData = $app->request->getJsonRawBody();
 
+    if (IsNullOrEmptyString($authData->Body)) {
+        throw new Exception('Body not informed');
+    }
+
+    if (IsNullOrEmptyString($authData->Secret)) {
+        throw new Exception('Secret not informed');
+    }
+
     return hash_hmac('sha1', $authData->Body, $authData->Secret);
 });
 
@@ -15,8 +23,18 @@ $app->get('/healthcheck', function () {
     echo 'ok';
 });
 
+$app->get('/', function () {
+    echo 'webhook-auth';
+});
+
 $app->notFound(function () use ($app) {
     $app->response->setStatusCode(404, "Not Found")->sendHeaders();
 });
 
 $app->handle();
+
+function IsNullOrEmptyString($str){
+    return (!isset($str) || trim($str) === '');
+}
+
+?>
