708237b14c5f36e38b4c5f489718b39dc68e896b
Heber Mattos <hebermattos@outlook.com>
2018-07-19 15:38:17 -0300

    new code

--- features/boletoaccount.feature
+++
@@ -1,7 +0,0 @@
-Feature: Boleto account
-  
-  Scenario: Receive Boleto Paid Event from Boleto Simples with invalid token
-    Given a valid boleto paid event payload
-    And with a "INVALID" token
-    When i do a POST against http://bswebhook-com.umbler.net/index.php?_url=/bswebhook
-    Then i should have "NOT AUTHORIZED" in response
--- index.php
+++ index.php
@@ -1,63 +1,18 @@
 <?php
 
-require 'vendor/autoload.php';
-require 'ContainerBuilder.php';
-
 use Phalcon\Mvc\Micro;
 
-use GuzzleHttp\Exception\ClientException;
-use GuzzleHttp\Exception\ServerException;
-use GuzzleHttp\Exception\ConnectException;
-
-$di = ContainerBuilder::Build();
-
 $app = new Micro();
-$app->setDI($di);
 
-$app->before(function () use ($app) {
-    
-    $header = $app->request->getHeader('HTTP_X_HUB_SIGNATURE');
-    $rawBody = $app->request->getRawBody();
-    $hashedBody = hash_hmac('sha1', $rawBody, $app->config->environment->secret);
+$app->post('/webhook-auth', function () use ($app) {
 
-    if(strcmp($header, 'sha1='.$hashedBody) != 0)
-    {
-        $app->response->setJsonContent(array('status' => 'NOT AUTHORIZED', 'data'=> null));
-        $app->response->setStatusCode(401);
-        $app->response->send();
-        
-        return false;
-    }
-    
-    return true;
+    $authData = $app->request->getJsonRawBody();
+
+    return hash_hmac('sha1', $authData->Body, $authData->Secret);
 });
 
-$app->post('/bswebhook', function () use ($app) {
-
-    try {
-        
-        $options = ['json' => $app->request->getJsonRawBody(),  'Authorization' => ['Basic '.$app->config->environment->token] ];
-        $data = $app->client->request('POST', $app->config->environment->url, $options)->getBody()->getContents();
-        
-        $app->response->setJsonContent(array('status' => "OK", 'data' => $data));
-        $app->response->setStatusCode(201);
-        
-    } catch (ServerException $e) {
-        $app->response->setJsonContent(array('status' => "INTERNAL SERVER ERROR", 'data' => $e->getResponse()->getBody()->getContents()));
-        $app->response->setStatusCode(500);
-    } catch (ConnectException $e) {
-        $app->response->setJsonContent(array('status' => "NOT FOUND", 'data' => $e->getResponse()));
-        $app->response->setStatusCode(404);
-    } catch (ClientException $e) {
-        $app->response->setJsonContent(array('status' => "BAD REQUEST", 'data' => $e->getResponse()->getBody()->getContents()));
-        $app->response->setStatusCode(400);
-    }
-    
-    return $app->response;
-});
-
-$app->get('/', function () {
-    echo 'bswebhook';
+$app->get('/healthcheck', function () {
+    echo 'ok';
 });
 
 $app->notFound(function () use ($app) {
